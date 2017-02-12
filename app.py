6d57274702e7a81547df7fbff456d1927d507b2c
David Zilbrug <davidzilburg@Davids-MacBook-Pro.local>
2017-02-11 23:23:54 -0500

    mongo connection and signin

--- app.py
+++ app.py
@@ -1,22 +1,44 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, json
 import jinja2
 import os
+from utils import *
+from pymongo import *
 
 app = Flask(__name__)
 
+client = MongoClient(os.environ.get('MONGO_URL'))
+db = client['listsapp']
+users = db['Users']
+
 @app.route('/')
 def hello():
+	return render_template("index.html")
+
+
+@app.route('/lists')
+def lists():
 	return render_template("lists.html")
 
-@app.route('/change')
-def chance():
-	return redirect('/')
+@app.route('/signIn', methods=['POST'])
+def signIn():
+	username = request.form.get('username')
+	password = request.form.get('password')
 
-@app.route('/post', methods=['GET','POST'])
-def post():
-	if request.method == 'POST':
-		return render_template('post.html')
-	return render_template('get.html')
+	if not(username):
+		return json.jsonify(error="No username given.", status="error")
+	if not(password):
+		return json.jsonify(status="error", error="No password given.")
+
+	username = username.lower()
+	user = users.find_one({'username':username})
+
+	if user is None:
+		return json.jsonify(status="error", error="No account found!")
+	if not(valid_pw(username,password,user.get('password'))):
+		return json.jsonify(status="error", error="Invalid username or password.")
+
+	session_login(username, user.get('name'))
+	return json.jsonify(status="success")
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 8000))
